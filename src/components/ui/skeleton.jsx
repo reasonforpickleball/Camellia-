699808c04dbdf0d6a21d20840ec2224acf6181b5
reasonforpickleball <achong2026@gmail.com>
2026-06-28 13:07:19 -0700

    Refactor Empty component to Skeleton component

--- src/components/ui/skeleton.jsx
+++ src/components/ui/skeleton.jsx
@@ -1,1 +1,14 @@
-export default function Empty(){return null;}
+import { cn } from "@/lib/utils"
+
+function Skeleton({
+  className,
+  ...props
+}) {
+  return (
+    (<div
+      className={cn("animate-pulse rounded-md bg-primary/10", className)}
+      {...props} />)
+  );
+}
+
+export { Skeleton }
