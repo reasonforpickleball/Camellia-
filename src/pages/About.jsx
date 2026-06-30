3b40f53b0c78b20db6d34b4c9b36140f706aeeba
reasonforpickleball <achong2026@gmail.com>
2026-06-29 19:50:58 -0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,3 @@
 ```javascript
-import React from 'react';
-
-function About() {
-  return (
-    <div style={{ padding: '40px' }}>
-      <h1>About</h1>
-      <p>Camellia study platform.</p>
-    </div>
-  );
-}
-
-export default About;
+export default function About() { return <div>About</div>; }
 ```
